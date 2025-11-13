59416062fa0c5105cce821a733b9cde0c6b0aaf9
arthur-adjedj <arthur.adjedj@gmail.com>
2025-11-13 17:00:18 +0100

    chore: more cleanups

--- Leanduction.lean
+++ Leanduction.lean
@@ -1,4 +1,2 @@
--- This module serves as the root of the `Leanduction` library.
--- Import modules here that should be built as part of the library.
 import Leanduction.NestedPositivity
 import Leanduction.SparseParametricity
